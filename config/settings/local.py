06fd33518e61e1871f4745c90cf116c4e4ec7312
Romain J <romain.ordi@gmail.com>
2021-02-05 01:43:35 +0100

    feat(style): done footer

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="w5OViLr9fy08dpPfvAflYDUKqT8SIlOP08qQEIRuVo8Ow92a7vT91ZEawvq3WffC",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "1a48d1f0d3b6.ngrok.io"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "9507b94c89da.ngrok.io"]
 
 # CACHES
 # ------------------------------------------------------------------------------
@@ -58,3 +58,6 @@ INSTALLED_APPS += ["django_extensions"]  # noqa F405
 
 # Your stuff...
 # ------------------------------------------------------------------------------
+# COMPRESS_OFFLINE = True
+# COMPRESS_ENABLED = True
+# COMPRESS_CSS_FILTERS = ['compressor.filters.css_default.CssAbsoluteFilter',  'compressor.filters.cssmin.CSSMinFilter']
