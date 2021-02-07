3876e87e320d741fc8f25b5586ca56eafe14c012
Romain J <romain.ordi@gmail.com>
2021-02-07 19:42:51 +0100

    feat(style): add .active on navbar

--- config/urls.py
+++ config/urls.py
@@ -9,7 +9,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
+        "sources/", TemplateView.as_view(template_name="pages/sources.html"), name="sources"
     ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
