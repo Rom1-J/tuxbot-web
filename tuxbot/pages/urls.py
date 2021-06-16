260fa7015dda9051d17fea217bd5b3780bf4b200
Romain J <romain.ordi@gmail.com>
2021-06-17 00:02:56 +0200

    second commit

--- tuxbot/pages/urls.py
+++ tuxbot/pages/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.views.generic import RedirectView
 
 from tuxbot.pages.views import HomeView, WikiView
@@ -6,7 +6,14 @@ from tuxbot.pages.views import HomeView, WikiView
 app_name = "pages"
 urlpatterns = [
     path("", view=HomeView.as_view(), name="home"),
+
     path("wiki/", view=WikiView.as_view(), name="wiki"),
-    path("support/", view=RedirectView.as_view(url='https://discord.gg/URKy7yd'), name="support"),
-    path("sources/", view=RedirectView.as_view(url='https://github.com/Rom1-J/tuxbot-bot'), name="sources"),
+    re_path(r"^wiki/(?P<path>[a-zA-Z0-9/\.~\-]+)$", view=WikiView.as_view(), name="wiki"),
+
+    path("support/",
+         view=RedirectView.as_view(url='https://discord.gg/URKy7yd'),
+         name="support"),
+    path("sources/",
+         view=RedirectView.as_view(url='https://github.com/Rom1-J/tuxbot-bot'),
+         name="sources"),
 ]
--- tuxbot/pages/views.py
+++ tuxbot/pages/views.py
@@ -1,7 +1,8 @@
+from django.conf import settings
 from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
-
 
 template_path = "pages/"
 
@@ -14,7 +15,25 @@ class HomeView(View):
 
 
 class WikiView(View):
-    template_name = template_path + "wiki.html"
+    template_name = template_path + "wiki/build/"
 
-    def get(self, request: WSGIRequest):
-        return render(request, self.template_name)
+    def get(self, request: WSGIRequest, path=None):
+        file_path = settings.ROOT_DIR / "tuxbot/templates"
+        content_type = "text/html"
+
+        if path is None:
+            file_path /= self.template_name + "index.html"
+        elif len(path.split('.')) == 1:
+            file_path /= self.template_name + path + "/index.html"
+        else:
+            file_path /= self.template_name + path
+
+            if (ext := path.split('.')[-1]) == "js":
+                content_type = f"text/javascript"
+            elif ext in ["svg", "png", "jpg", "jpeg"]:
+                content_type = f"image/{ext}"
+            else:
+                content_type = f"text/{ext}"
+
+        with open(file_path, "rb") as f:
+            return HttpResponse(f.read(), content_type=content_type)
